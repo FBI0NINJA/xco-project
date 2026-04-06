177ff2592632aaaefd66dccd5434087484f8050a
Ahmed Sayed <88555626+FBI0NINJA@users.noreply.github.com>
2026-04-06 13:18:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ Location: ${user.location}
 --------------------------
 `;
 
-    fs.appendFileSync("users.txt", line);
-
-    res.send("Saved");
-});
-
 const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log("Server running"));
